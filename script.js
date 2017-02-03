eb449b70fab515d2f36786d794ef0275f2113732
Nick <nickjanne7@gmail.com>
2017-02-03 16:36:55 -0500

    Updated progress

--- script.js
+++ script.js
@@ -1,45 +1,19 @@
-$(document).ready(function () {
-
-  function buttonOneHit () {
-  $('div.buttonOne').attr('id', 'buttonOneHit');
-  setTimeout($('div.buttonOne').removeAttr('id', 'buttonOneHit'), 500);
-  }
-
-  function buttonTwoHit () {
-  $('div.buttonTwo').attr('id', 'buttonTwoHit');
-  setTimeout($('div.buttonTwo').removeAttr('id', 'buttonTwoHit'), 500);
-  }
-
-  function buttonThreeHit () {
-  $('div.buttonThree').attr('id', 'buttonThreeHit');
-  setTimeout($('div.buttonThree').removeAttr('id', 'buttonThreeHit'), 500);
-  }
-
-  function buttonFourHit () {
-  $('div.buttonFour').attr('id', 'buttonFourHit');
-  setTimeout($('div.buttonFour').removeAttr('id', 'buttonFourHit'), 500);
-  }
-
-var choices = [3, 4, 2, 1, 4, 3];
-
-function displaySequence () {
-  for (var i = 0; i<choices.length; i++) {
-    if (choices[i] == 1) {
-      buttonOneHit();
-      return;
-    } else if (choices[i] == 2) {
-      buttonTwoHit();
-      return;
-    } else if (choices[i] == 3) {
-      buttonThreeHit();
-      return;
-    } else if (choices[i] == 4) {
-      buttonFourHit();
-      return;
-    }
-  }
+function buttonOneHit () {
+$('div.buttonOne').attr('id', 'buttonOneHit');
 }
 
-displaySequence();
+function buttonTwoHit () {
+$('div.buttonTwo').attr('id', 'buttonTwoHit');
+}
+
+function buttonThreeHit () {
+$('div.buttonThree').attr('id', 'buttonThreeHit');
+}
+
+function buttonFourHit () {
+$('div.buttonFour').attr('id', 'buttonFourHit');
+}
+$(document).ready(function() {
+
 
 });
