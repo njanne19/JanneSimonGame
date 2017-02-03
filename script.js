f1cbeb33d5c1329896344334c4b9ddf99319c543
Nick <nickjanne7@gmail.com>
2017-02-03 17:29:16 -0500

    Reacts to an array

--- script.js
+++ script.js
@@ -16,4 +16,44 @@ $('div.buttonFour').attr('id', 'buttonFourHit');
 $(document).ready(function() {
 
 
+ var sequence = [1, 4, 3, 1, 2, 4, 3, 2, 1, 4, 2, 1, 3, 1, 2];
+
+function newRound() {
+ animate(sequence);
+}
+function animate(sequence) {
+ var i = 0;
+ var interval = setInterval(function() {
+ lightUp(sequence[i]);
+        i++;
+        if (i >= sequence.length) {
+ clearInterval(interval);
+        }
+   }, 600);
+}
+function lightUp(tile) {
+  var rightBlock;
+  switch(tile) {
+    case 1:
+    rightBlock = 'buttonOneHit';
+    break;
+    case 2:
+    rightBlock = 'buttonTwoHit';
+    break;
+    case 3:
+    rightBlock = 'buttonThreeHit';
+    break;
+    case 4:
+    rightBlock = 'buttonFourHit';
+    break;
+  }
+ var $tile = $('[data-tile=' + tile + ']').attr('id', rightBlock);
+ window.setTimeout(function() {
+ $tile.removeAttr('id', rightBlock);
+ }, 300);
+}
+
+
+newRound();
+
 });
