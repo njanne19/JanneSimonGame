e590bf95727a5c00b35ff1c11f869fbd45296c25
Nick <nickjanne7@gmail.com>
2017-02-03 11:48:36 -0500

    Make's a class and functions that will react to an array, however does not react to an array just yet

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 $(document).ready(function () {
+
+  function buttonOneHit () {
+  $('div.buttonOne').attr('id', 'buttonOneHit');
+  }
+
+  function buttonTwoHit () {
+  $('div.buttonTwo').attr('id', 'buttonTwoHit');
+  }
+
+  function buttonThreeHit () {
+  $('div.buttonThree').attr('id', 'buttonThreeHit');
+  }
+
+  function buttonFourHit () {
+  $('div.buttonFour').attr('id', 'buttonFourHit');
+  }
+
+var choices = [3, 4, 2, 1, 4, 3];
+
 });
